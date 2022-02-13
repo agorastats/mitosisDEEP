3beb07185e60d537bf3edc0638da38a460fd2d81
lgnum-aml <amercader@lgnum.eu>
2022-02-13 21:40:32 +0100

    utils keras
    Signed-off-by: lgnum-aml <amercader@lgnum.eu>

---
+++ utils/keras/callbacks.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+import tensorflow as tf
+import shutil
+
+from utils.image import create_dir
+from utils.loadAndSaveResults import store_data_frame
+
+LOGS_DRIVE_PATH = '/content/drive/MyDrive/mitosis_data/logs/'
+WEIGHTS_DRIVE_PATH = '/content/drive/MyDrive/mitosis_data'
+LOGS_CSV_PATH = 'logsCSV'
+
+def early_stop(monitor='val_loss', patience=20):
+    # early stop if patience epochs not improve monitor metric/loss
+    return tf.keras.callbacks.EarlyStopping(monitor=monitor, patience=patience)
+
+
+def reduce_lr(monitor='val_loss', factor=0.2, patience=10, min_lr=0.000001, verbose=1):
+    # reduce learning rate by factor if not improve monitor in patience epochs
+    return tf.keras.callbacks.ReduceLROnPlateau(monitor=monitor, factor=factor,
+                                                patience=patience, min_lr=min_lr, verbose=verbose)
+
+
+def checkpoint(output_name, monitor='val_loss', verbose=1,  mode='min',
+               save_best_only=True, save_weights_only=True):
+    # output_path: to save information about model, for example weights
+    output_path = os.path.join(WEIGHTS_DRIVE_PATH, '%s.h5' % str(output_name))
+    return tf.keras.callbacks.ModelCheckpoint(output_path, monitor=monitor, verbose=verbose,
+                                              save_best_only=save_best_only, mode=mode,
+                                              save_weights_only=save_weights_only)
+
+
+def csv_logger(output_name, append=True, separator=';'):
+    # callback to save logs of epochs model as csv
+    output_path = os.path.join(LOGS_CSV_PATH, '%s.csv' % str(output_name))
+    store_data_frame(pd.DataFrame(), output_path)
+    return tf.keras.callbakcs.CSVLogger(output_path, append=append, separator=separator)
+
+
+class UpdateLoggerToDrive(tf.keras.callbacks.Callback):
+    def __init__(self, N, output_name, output_drive=LOGS_DRIVE_PATH):
+        self.N = N
+        self.epoch = 0
+        self.output_name = str(output_name)
+        create_dir(LOGS_CSV_PATH)
+        self.logCSV = os.path.join(LOGS_CSV_PATH, '%s.csv' % str(output_name))
+        self.output_drive = os.path.join(output_drive, '%s.csv' % self.output_name)
+
+    def on_batch_end(self, epoch, logs={}):
+        if self.epoch % self.N == 0:
+          shutil.copy(self.logCSV, self.output_drive)
+        self.epoch += 1
+
+
+def get_callbacks_list(output_name, N=5):
+    return [early_stop(), reduce_lr(), checkpoint(output_name),
+            csv_logger(output_name), UpdateLoggerToDrive(N, output_name)]
+
+
---
+++ utils/keras/lossAndMetrics.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from keras.losses import binary_crossentropy
+
+# ref: https://github.com/nauyan/Segmentation/blob/master/Code/utils/lossfunctions.py
+# ref: https://github.com/nauyan/Segmentation/blob/master/Code/utils/metricfunctions.py
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + K.epsilon()) / (K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
+
+
+def dice_loss(y_true, y_pred):
+    numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1, 2, 3))
+    denominator = tf.reduce_sum(y_true + y_pred, axis=(1, 2, 3))
+    return 1 - numerator / denominator
+
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+
+def jaccard_loss(y_true, y_pred, smooth=100):
+    """
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+
+    The jaccard distance loss is usefull for unbalanced datasets. This has been
+    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+    gradient.
+
+    Ref: https://en.wikipedia.org/wiki/Jaccard_index
+
+    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+    @author: wassname
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
